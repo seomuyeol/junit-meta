1d2835bb9d345304d996d1000c39a9e71f04e284
seomy <muyear.seo@gmail.com>
2022-10-17 13:53:15 +0900

    service 登録Test

--- src/test/java/com/junitP/junit/service/BookServiceTest.java
+++ src/test/java/com/junitP/junit/service/BookServiceTest.java
@@ -1,5 +1,39 @@
 package com.junitP.junit.service;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.junitP.junit.domain.BookRepository;
+import com.junitP.junit.util.MailSenderStub;
+import com.junitP.junit.web.dto.BookRespDto;
+import com.junitP.junit.web.dto.BookSaveReqDto;
+
+@DataJpaTest
 public class BookServiceTest {
 
+	@Autowired
+	private BookRepository bookRepository;
+	
+	@Test
+	public void bookSaveTest() {
+		//given
+		BookSaveReqDto dto = new BookSaveReqDto();
+		dto.setTitle("junit-seo");
+		dto.setAuthor("meta-seo");
+		
+		//stub
+		
+		//when
+		MailSenderStub mailSenderStub = new MailSenderStub();
+		BookService bookService = new BookService(bookRepository, mailSenderStub);
+		BookRespDto bookRespDto = bookService.bookSave(dto);
+		
+		//then
+		assertEquals(dto.getTitle(), bookRespDto.getTitle());
+		assertEquals(dto.getAuthor(), bookRespDto.getAuthor());
+
+	}
 }
