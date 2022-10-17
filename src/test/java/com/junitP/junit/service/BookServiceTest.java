3b0ed1930ce9713eaf9b5eb3e64d777b8ecfaa11
seomy <muyear.seo@gmail.com>
2022-10-17 14:40:55 +0900

    service saveTest assertj

--- src/test/java/com/junitP/junit/service/BookServiceTest.java
+++ src/test/java/com/junitP/junit/service/BookServiceTest.java
@@ -1,22 +1,35 @@
 package com.junitP.junit.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.junitP.junit.domain.BookRepository;
-import com.junitP.junit.util.MailSenderStub;
+import com.junitP.junit.util.MailSender;
 import com.junitP.junit.web.dto.BookRespDto;
 import com.junitP.junit.web.dto.BookSaveReqDto;
 
-@DataJpaTest
+//@DataJpaTest //Tranjectional あるため、テスト後ロールバックします。
+@ExtendWith(MockitoExtension.class)
 public class BookServiceTest {
-
-	@Autowired
-	private BookRepository bookRepository;
 	
+	@InjectMocks
+	private BookService bookService;
+	@Mock
+	private BookRepository bookRepository;
+	@Mock
+	private MailSender mailSender;
+
+//	@Autowired
+//	private BookRepository bookRepository;
+	
+	// 気になる…　サービスだけテストしたいけど、レポジトリもテストになる　Mockito使用
 	@Test
 	public void bookSaveTest() {
 		//given
@@ -24,16 +37,19 @@ public class BookServiceTest {
 		dto.setTitle("junit-seo");
 		dto.setAuthor("meta-seo");
 		
-		//stub
+		//stub（行動定義）
+		when(bookRepository.save(any())).thenReturn(dto.toEntity());
+		when(mailSender.send()).thenReturn(true);
 		
 		//when
-		MailSenderStub mailSenderStub = new MailSenderStub();
-		BookService bookService = new BookService(bookRepository, mailSenderStub);
 		BookRespDto bookRespDto = bookService.bookSave(dto);
 		
 		//then
-		assertEquals(dto.getTitle(), bookRespDto.getTitle());
-		assertEquals(dto.getAuthor(), bookRespDto.getAuthor());
+//		assertEquals(dto.getTitle(), bookRespDto.getTitle());
+//		assertEquals(dto.getAuthor(), bookRespDto.getAuthor());
+		assertThat(dto.getTitle()).isEqualTo(bookRespDto.getTitle());
+		assertThat(dto.getAuthor()).isEqualTo(bookRespDto.getAuthor());
+		
 
 	}
 }
